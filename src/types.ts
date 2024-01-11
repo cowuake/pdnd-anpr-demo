b78caa5d808e261c4fb15a52579fa2cf03d6c633
r-mura <riccardo_mura@yahoo.com>
2024-01-11 09:27:08 +0100

    Applied more consistent style

--- src/types.ts
+++ src/types.ts
@@ -1,37 +1,37 @@
 export interface Config {
-  authURL: string
-  clientID: string
-  codiceFiscale: string
-  hashAlgorithm: string
-  keyID: string
-  keyLocation: string
-  LoA: string
-  purposeID: string
-  serviceAudience: string
-  serviceURL: string
-  signatureAudience: string
-  tokenValidityInMinutes: number
-  userID: string
-  userLocation: string
-  verbose: boolean
+  authURL: string;
+  clientID: string;
+  codiceFiscale: string;
+  hashAlgorithm: string;
+  keyID: string;
+  keyLocation: string;
+  LoA: string;
+  purposeID: string;
+  serviceAudience: string;
+  serviceURL: string;
+  signatureAudience: string;
+  tokenValidityInMinutes: number;
+  userID: string;
+  userLocation: string;
+  verbose: boolean;
 }
 
 export interface AuthResponseBody {
-  access_token: string
-  expires_in: number
-  token_type: string
+  access_token: string;
+  expires_in: number;
+  token_type: string;
 }
 
 export interface EivRequestBody {
-  idOperazioneClient: string
+  idOperazioneClient: string;
   criteriRicerca: {
-    codiceFiscale: string
-  }
+    codiceFiscale: string;
+  };
   datiRichiesta: {
-    dataRiferimentoRichiesta: string
-    casoUso: string
-    motivoRichiesta: string
-  }
+    dataRiferimentoRichiesta: string;
+    casoUso: string;
+    motivoRichiesta: string;
+  };
 }
 
 export interface EivResponseBody {
@@ -39,31 +39,31 @@ export interface EivResponseBody {
     datiSoggetto: Array<{
       generalita: {
         codiceFiscale: {
-          codFiscale: string
-          validitaCF: string
-        }
-        cognome: string
-        dataNascita: string
-        idSchedaSoggettoANPR: string
+          codFiscale: string;
+          validitaCF: string;
+        };
+        cognome: string;
+        dataNascita: string;
+        idSchedaSoggettoANPR: string;
         luogoNascita: {
           comune: {
-            codiceIstat: string
-            nomeComune: string
-            siglaProvinciaIstat: string
-          }
-        }
-        nome: string
-        sesso: string
-      }
+            codiceIstat: string;
+            nomeComune: string;
+            siglaProvinciaIstat: string;
+          };
+        };
+        nome: string;
+        sesso: string;
+      };
       identificativi: {
-        idANPR: string
-      }
+        idANPR: string;
+      };
       infoSoggettoEnte: Array<{
-        chiave: string
-        id: string
-        valore: string
-      }>
-    }>
-  }
-  idOperazioneANPR: string
+        chiave: string;
+        id: string;
+        valore: string;
+      }>;
+    }>;
+  };
+  idOperazioneANPR: string;
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,29 +1,29 @@
-import * as crypto from 'crypto'
-import { type Config } from './types'
-import { type JwtPayload } from 'jsonwebtoken'
+import * as crypto from 'crypto';
+import { type Config } from './types';
+import { type JwtPayload } from 'jsonwebtoken';
 
 export function generateJti(): string {
-  return crypto.randomUUID()
+  return crypto.randomUUID();
 }
 
 export function generateDnonce(): string {
-  return crypto.randomUUID().replace('-', '')
+  return crypto.randomUUID().replace('-', '');
 }
 
 export function computeEpochTime(date: Date): number {
-  return Math.floor(date.getTime() / 1000)
+  return Math.floor(date.getTime() / 1000);
 }
 
 export function computeJwtValidityInterval(deltaInMinutes: number): [number, number] {
-  const issueTime = new Date()
-  const expirationTime = new Date()
-  expirationTime.setTime(issueTime.getTime() + deltaInMinutes * 60 * 1000)
+  const issueTime = new Date();
+  const expirationTime = new Date();
+  expirationTime.setTime(issueTime.getTime() + deltaInMinutes * 60 * 1000);
 
-  return [computeEpochTime(issueTime), computeEpochTime(expirationTime)]
+  return [computeEpochTime(issueTime), computeEpochTime(expirationTime)];
 }
 
 export function buildAuditJwtPayload(config: Config): JwtPayload {
-  const [issued, expires] = computeJwtValidityInterval(config.tokenValidityInMinutes)
+  const [issued, expires] = computeJwtValidityInterval(config.tokenValidityInMinutes);
   return {
     sub: config.clientID,
     jti: generateJti(),
@@ -37,11 +37,14 @@ export function buildAuditJwtPayload(config: Config): JwtPayload {
     exp: expires,
     iss: config.clientID,
     aud: config.signatureAudience
-  }
+  };
 }
 
-export function buildClientAssertionJwtPayload(encodedAuditJwtToken: string, config: Config): JwtPayload {
-  const [issued, expires] = computeJwtValidityInterval(config.tokenValidityInMinutes)
+export function buildClientAssertionJwtPayload(
+  encodedAuditJwtToken: string,
+  config: Config
+): JwtPayload {
+  const [issued, expires] = computeJwtValidityInterval(config.tokenValidityInMinutes);
   return {
     sub: config.clientID,
     jti: generateJti(),
@@ -55,32 +58,36 @@ export function buildClientAssertionJwtPayload(encodedAuditJwtToken: string, con
     exp: expires,
     iss: config.clientID,
     aud: config.serviceAudience
-  }
+  };
 }
 
 export function buildIntegrityJwtPayload(digest: string, config: Config): JwtPayload {
-  const [issued, expires] = computeJwtValidityInterval(config.tokenValidityInMinutes)
+  const [issued, expires] = computeJwtValidityInterval(config.tokenValidityInMinutes);
 
   return {
     sub: config.clientID,
     jti: generateJti(),
     iat: issued,
-    signed_headers: [{ digest }, { Accept: 'application/json' }, { 'Content-Type': 'application/json' }],
+    signed_headers: [
+      { digest },
+      { Accept: 'application/json' },
+      { 'Content-Type': 'application/json' }
+    ],
     nbf: issued,
     exp: expires,
     iss: config.clientID,
     aud: config.signatureAudience
-  }
+  };
 }
 
 export function logVerbose(label: string, value: string | object, config: Config): void {
   if (config.verbose) {
     if (value instanceof String) {
-      console.log(`${label}:\n\t${value}\n`)
+      console.log(`${label}:\n\t${value}\n`);
     } else {
-      console.log(`======= ${label} =======`)
-      console.log(JSON.stringify(value, null, 4))
-      console.log()
+      console.log(`======= ${label} =======`);
+      console.log(JSON.stringify(value, null, 4));
+      console.log();
     }
   }
 }
